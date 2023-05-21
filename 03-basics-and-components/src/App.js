0776fb73b7ae75fc4a9ffed56fda3e1a902ccb2c
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2023-05-21 22:15:02 +0100

    changed some css

--- 03-basics-and-components/src/App.js
+++ 03-basics-and-components/src/App.js
@@ -1,9 +1,18 @@
-function App() {
+// function App() {
+//   return (
+//     <div>
+//       <h2>Let's get started it started in hot!</h2>
+//     </div>
+//   );
+// }
+
+const App = () => {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h2>Lets get it started it here!</h2>
     </div>
-  );
-}
+
+    )
+  }
 
 export default App;
